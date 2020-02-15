6033aea9db99102e382dfc6a5e858bcb8d692159
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-02-15 22:39:29 +0100

    :sparkles: read log from csv file

--- src/com/mrozwadowski/fuzzyminer/CLI.kt
+++ src/com/mrozwadowski/fuzzyminer/CLI.kt
@@ -1,5 +1,7 @@
 package com.mrozwadowski.fuzzyminer
 
+import com.mrozwadowski.fuzzyminer.data.log.Log
+import com.mrozwadowski.fuzzyminer.input.CSV
 import java.io.File
 import java.io.IOException
 import kotlin.system.exitProcess
@@ -11,11 +13,14 @@ fun main(args: Array<String>) {
     }
     val path = args[0]
 
+    var log: Log
     try {
         val file = File(path)
-        println(file)
+        log = CSV(file).readLog()
     } catch (e: IOException) {
         System.err.println("Failed to open $path for reading")
         exitProcess(1)
     }
+
+    println(log)
 }
---
+++ src/com/mrozwadowski/fuzzyminer/input/CSV.kt
@@ -0,0 +1,45 @@
+package com.mrozwadowski.fuzzyminer.input
+
+import com.mrozwadowski.fuzzyminer.data.log.Activity
+import com.mrozwadowski.fuzzyminer.data.log.Log
+import com.mrozwadowski.fuzzyminer.data.log.Trace
+import java.io.File
+
+class CSV(private val file: File) {
+    private var traceIdColumn: Int = -1
+    private var activityColumn: Int = -1
+    private var activities = HashMap<String, Activity>();
+    private var traces = HashMap<String, ArrayList<Activity>>();
+
+    fun readLog() : Log {
+        readFile()
+        return Log(traces.values.map { Trace(it) })
+    }
+
+    private fun readFile() {
+        var headersRead = false
+        file.forEachLine { line ->
+            val fields = line.split(",")
+            if (!headersRead) {
+                readHeaders(fields)
+                headersRead = true
+            } else {
+                readActivity(fields)
+            }
+        }
+    }
+
+    private fun readHeaders(fields: List<String>) {
+        traceIdColumn = fields.indexOf("Case ID")
+        activityColumn = fields.indexOf("Activity")
+    }
+
+    private fun readActivity(fields: List<String>) {
+        val traceId = fields[traceIdColumn]
+        val activityName = fields[activityColumn]
+
+        val activity = activities.getOrPut(activityName, { Activity(activityName) })
+        val trace = traces.getOrPut(traceId, { arrayListOf() })
+        trace.add(activity)
+    }
+}
