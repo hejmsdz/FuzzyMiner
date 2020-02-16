dc522b58e787b46da010f5ec56365adaf1701733
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-02-16 12:08:06 +0100

    :recycle: refactor graph construction

--- src/com/mrozwadowski/fuzzyminer/mining/DumbMiner.kt
+++ src/com/mrozwadowski/fuzzyminer/mining/DumbMiner.kt
@@ -8,12 +8,25 @@ import com.mrozwadowski.fuzzyminer.data.log.Log
 
 class DumbMiner(private val log: Log) {
     fun mine(): Graph {
-        val nodes = log.activities.associateWith { Node(it.name, it.id) }
-        val edges = findSuccessions().keys
-            .groupBy { it.first }
-            .map { nodes[it.key]!! to it.value.map { Edge(nodes[it.second]!!) } }
-            .toMap()
-        return Graph(nodes.values.toList(), edges)
+        val activitiesToNodes = getActivitiesToNodes()
+        val nodes = activitiesToNodes.values.toList()
+        val edges = getEdges(activitiesToNodes)
+        return Graph(nodes, edges)
+    }
+
+    private fun getActivitiesToNodes(): Map<Activity, Node> {
+        return log.activities.associateWith { Node(it.name, it.id) }
+    }
+
+    private fun getEdges(activitiesToNodes: Map<Activity, Node>): Map<Node, List<Edge>> {
+        val edges = mutableMapOf<Node, MutableList<Edge>>()
+        findSuccessions().forEach { (activities, _) ->
+            val sourceNode = activitiesToNodes.getValue(activities.first)
+            val targetNode = activitiesToNodes.getValue(activities.second)
+            val edgesForNode = edges.getOrPut(sourceNode, { mutableListOf() })
+            edgesForNode.add(Edge(targetNode))
+        }
+        return edges
     }
 
     private fun findSuccessions(): Map<Pair<Activity, Activity>, Int> {
@@ -21,7 +34,9 @@ class DumbMiner(private val log: Log) {
         log.traces.forEach { trace ->
             val length = trace.events.size
             for (i in 1 until length) {
-                val pair = Pair(trace.events[i - 1].activity, trace.events[i].activity)
+                val event1 = trace.events[i - 1]
+                val event2 = trace.events[i]
+                val pair = event1.activity to event2.activity
                 successions.putIfAbsent(pair, 0)
                 successions[pair]!!.inc()
             }
