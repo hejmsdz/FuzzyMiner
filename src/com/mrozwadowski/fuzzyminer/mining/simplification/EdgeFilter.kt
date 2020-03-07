296681e788c5a4d8f8e3ad22d815a5f3ea52ed1d
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-03-07 22:54:09 +0100

    :sparkles: add utility-base edge filter

--- src/com/mrozwadowski/fuzzyminer/mining/simplification/EdgeFilter.kt
+++ src/com/mrozwadowski/fuzzyminer/mining/simplification/EdgeFilter.kt
@@ -1,6 +1,8 @@
 package com.mrozwadowski.fuzzyminer.mining.simplification
 
+import com.mrozwadowski.fuzzyminer.data.graph.Edge
 import com.mrozwadowski.fuzzyminer.data.graph.Graph
+import com.mrozwadowski.fuzzyminer.data.graph.Node
 import com.mrozwadowski.fuzzyminer.mining.metrics.BinaryCorrelationMetric
 import com.mrozwadowski.fuzzyminer.mining.metrics.BinarySignificanceMetric
 
@@ -9,23 +11,33 @@ class EdgeFilter<EventClass>(
     private val binSignificance: BinarySignificanceMetric<EventClass>,
     private val binCorrelation: BinaryCorrelationMetric<EventClass>
 ) {
-    fun filterEdges(utilityRatio: Double, cutoff: Double) {
-        val edgesToRemove = graph.nodes.flatMap { source ->
-            val edges = graph.edgesFrom(source)
-            if (edges.isEmpty()) {
-                return@flatMap listOf()
-            }
-            val utilities = edges.associateWith {
-                utility(source.eventClass, it.target.eventClass, utilityRatio)
-            }
-            val min = utilities.values.min()!!
-            val max = utilities.values.max()!!
-            val range = max - min
-            utilities
-                .filterValues { ((it - min) / range) < cutoff }
-                .keys
-                .map { source to it.target }
+    fun filterEdges(utilityRatio: Double, cutoff: Double): Graph<EventClass> {
+        val allEdges = graph.allEdges()
+        val edgesToRemove = graph.nodes
+            .map { it to graph.edgesFrom(it) }
+            .filterNot { (_, edges) -> edges.isEmpty() }
+            .flatMap { (source, edges) ->
+                relativeUtilities(source, edges, utilityRatio)
+                    .filterValues { it < cutoff }
+                    .keys
+                    .map { source to it.target }
         }
+
+        val preservedEdges = (allEdges - edgesToRemove)
+            .groupBy { it.first }
+            .mapValues { it.value.map { (_, target) -> Edge(target) } }
+
+        return Graph(graph.nodes, preservedEdges)
+    }
+
+    fun relativeUtilities(source: Node<EventClass>, edges: Collection<Edge<EventClass>>, utilityRatio: Double): Map<Edge<EventClass>, Double> {
+        val utilities = edges.associateWith {
+            utility(source.eventClass, it.target.eventClass, utilityRatio)
+        }
+        val min = utilities.values.min()!!
+        val max = utilities.values.max()!!
+        val range = max - min
+        return utilities.mapValues { (it.value - min) / range }
     }
 
     private fun utility(a: EventClass, b: EventClass, utilityRatio: Double): Double {
--- test/com/mrozwadowski/fuzzyminer/mining/simplification/EdgeFilterTest.kt
+++ test/com/mrozwadowski/fuzzyminer/mining/simplification/EdgeFilterTest.kt
@@ -1,12 +1,61 @@
 package com.mrozwadowski.fuzzyminer.mining.simplification
 
+import com.mrozwadowski.fuzzyminer.data.graph.Edge
+import com.mrozwadowski.fuzzyminer.data.graph.Graph
+import com.mrozwadowski.fuzzyminer.data.graph.Node
+import com.mrozwadowski.fuzzyminer.data.log.Activity
+import com.mrozwadowski.fuzzyminer.mining.metrics.BinaryCorrelationMetric
+import com.mrozwadowski.fuzzyminer.mining.metrics.BinarySignificanceMetric
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
 internal class EdgeFilterTest {
+    val a = Node(Activity("a", 1), 1)
+    val p = Node(Activity("p", 2), 2)
+    val q = Node(Activity("q", 3), 3)
+    val r = Node(Activity("r", 4), 4)
+    val s = Node(Activity("s", 5), 5)
+    val graph = Graph(
+        listOf(a, p, q, r, s),
+        mapOf(a to listOf(Edge(p), Edge(q), Edge(r), Edge(s)))
+    )
+
+    private class MockSignificance: BinarySignificanceMetric<Activity>() {
+        val values = mapOf("p" to 1.0, "q" to 0.9, "r" to 0.7, "s" to 0.5)
+        override fun calculate(class1: Activity, class2: Activity): Number {
+            assert(class1.name == "a")
+            return values.getOrDefault(class2.name, 0)
+        }
+    }
+
+    private class MockCorrelation: BinaryCorrelationMetric<Activity>() {
+        val values = mapOf("p" to 1.0, "q" to 0.2, "r" to 0.8, "s" to 0.3)
+        override fun calculate(class1: Activity, class2: Activity): Number {
+            assert(class1.name == "a")
+            return values.getOrDefault(class2.name, 0)
+        }
+    }
+
+    private val edgeFilter = EdgeFilter(graph, MockSignificance(), MockCorrelation())
+    private val utilityRatio = 0.5
+    private val cutoff = 0.4
+
+    @Test
+    fun relativeUtilities() {
+        val relativeUtilities = edgeFilter.relativeUtilities(a, graph.edgesFrom(a), utilityRatio)
+        assertEquals(1.000, relativeUtilities[Edge(p)]!!, 0.001)
+        assertEquals(0.250, relativeUtilities[Edge(q)]!!, 0.001)
+        assertEquals(0.583, relativeUtilities[Edge(r)]!!, 0.001)
+        assertEquals(0.000, relativeUtilities[Edge(s)]!!, 0.001)
+    }
 
     @Test
     fun filterEdges() {
+        val simplifiedGraph = edgeFilter.filterEdges(utilityRatio, cutoff)
+        assertNotNull(simplifiedGraph.edgeBetween(a, p))
+        assertNull(simplifiedGraph.edgeBetween(a, q))
+        assertNotNull(simplifiedGraph.edgeBetween(a, r))
+        assertNull(simplifiedGraph.edgeBetween(a, s))
     }
 }
