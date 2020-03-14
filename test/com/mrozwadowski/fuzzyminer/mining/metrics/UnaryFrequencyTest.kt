b8a6266a123ace2a905485410b167642a2950941
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-03-14 18:06:02 +0100

    :white_check_mark: update test to use OpenXES

--- test/com/mrozwadowski/fuzzyminer/mining/metrics/UnaryFrequencyTest.kt
+++ test/com/mrozwadowski/fuzzyminer/mining/metrics/UnaryFrequencyTest.kt
@@ -1,31 +1,20 @@
 package com.mrozwadowski.fuzzyminer.mining.metrics
 
-import com.mrozwadowski.fuzzyminer.data.log.Activity
-import com.mrozwadowski.fuzzyminer.data.log.Event
-import com.mrozwadowski.fuzzyminer.data.log.Log
-import com.mrozwadowski.fuzzyminer.data.log.Trace
+import com.mrozwadowski.fuzzyminer.conceptName
+import com.mrozwadowski.fuzzyminer.createSimpleLog
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class UnaryFrequencyTest {
-    private val a = Activity("a", 0)
-    private val b = Activity("b", 1)
-    private val c = Activity("c", 2)
-    private val d = Activity("d", 3)
-    private val e = Activity("e", 4)
-    private val log = Log(listOf(
-        Trace(listOf(Event(a), Event(b), Event(d))),
-        Trace(listOf(Event(a), Event(c))),
-        Trace(listOf(Event(a), Event(b), Event(b), Event(b), Event(d)))
-    ))
+    private val log = createSimpleLog(listOf("abd", "ac", "abbbd"))
 
     @Test
     fun calculate() {
-        val metric = UnaryFrequency(log, Event::activity)
-        assertEquals(0.75, metric.calculate(a))
-        assertEquals(1.0, metric.calculate(b))
-        assertEquals(0.25, metric.calculate(c))
-        assertEquals(0.5, metric.calculate(d))
-        assertEquals(0.0, metric.calculate(e))
+        val metric = UnaryFrequency(log, ::conceptName)
+        assertEquals(0.75, metric.calculate("a"))
+        assertEquals(1.0, metric.calculate("b"))
+        assertEquals(0.25, metric.calculate("c"))
+        assertEquals(0.5, metric.calculate("d"))
+        assertEquals(0.0, metric.calculate("e"))
     }
 }
---
+++ test/com/mrozwadowski/fuzzyminer/testUtils.kt
@@ -0,0 +1,29 @@
+package com.mrozwadowski.fuzzyminer
+
+import org.deckfour.xes.factory.XFactoryNaiveImpl
+import org.deckfour.xes.model.XEvent
+import org.deckfour.xes.model.XLog
+
+fun createSimpleLog(traces: List<String>): XLog {
+    return createLog(traces.map { it.toCharArray().map(Char::toString) })
+}
+
+fun createLog(traces: List<List<String>>): XLog {
+    val factory = XFactoryNaiveImpl()
+    val log = factory.createLog()
+
+    traces.forEach { traceStr ->
+        val trace = factory.createTrace()
+        traceStr.forEach { eventStr ->
+            val event = factory.createEvent()
+            val key = "concept:name"
+            val conceptName = factory.createAttributeLiteral(key, eventStr, null)
+            event.attributes[key] = conceptName
+            trace.add(event)
+        }
+        log.add(trace)
+    }
+    return log
+}
+
+fun conceptName(event: XEvent) = event.attributes["concept:name"].toString()
