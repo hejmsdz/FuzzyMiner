0320f9658f0dddaa4d9cd296371ae0b9c6cda173
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-07-21 19:30:37 +0200

    :alembic: compare metrics calculated online and offline

--- src/com/mrozwadowski/fuzzyminer/experiments/OnlineWindowComparison.kt
+++ src/com/mrozwadowski/fuzzyminer/experiments/OnlineWindowComparison.kt
@@ -1,21 +1,55 @@
 package com.mrozwadowski.fuzzyminer.experiments
 
 import com.mrozwadowski.fuzzyminer.input.getLogReader
-import com.mrozwadowski.fuzzyminer.mining.metrics.defaultMetrics
+import com.mrozwadowski.fuzzyminer.mining.metrics.MetricsStore
+import com.mrozwadowski.fuzzyminer.mining.metrics.minimalMetrics
 import com.mrozwadowski.fuzzyminer.mining.online.OnlineFuzzyMiner
+import org.deckfour.xes.classification.XEventClasses
 import org.deckfour.xes.classification.XEventNameClassifier
 import java.io.File
 
 fun main() {
-    val log = getLogReader(File("sampleData/journal_review.xes")).readLog()
-    val window = SlidingWindow(log, windowSize = 20, stride = 5)
+    val log = getLogReader(File("sampleData/simple.csv")).readLog()
+    val window = SlidingWindow(log, windowSize = 3, stride = 2)
 
     val classifier = XEventNameClassifier()
-    val onlineMiner = OnlineFuzzyMiner(classifier, defaultMetrics())
+    val metrics = minimalMetrics()
+    val onlineMiner = OnlineFuzzyMiner(classifier, metrics)
 
     onlineMiner.learn(window.initial())
     window.steps().forEach { step ->
         onlineMiner.learn(window.incoming(step))
         onlineMiner.unlearn(window.outgoing(step))
     }
+
+    println(metrics.normalizationFactors)
+    printMetrics(metrics)
+
+    val lastFragment = window.fragment(window.steps().last)
+    val offlineMetrics = minimalMetrics()
+    offlineMetrics.calculateFromLog(lastFragment, XEventClasses.deriveEventClasses(classifier, lastFragment))
+    printMetrics(offlineMetrics)
+}
+
+fun printMetrics(metrics: MetricsStore) {
+    val dump = metrics.dumpMetrics()
+    dump.unarySignificance.keys.sortedWith(compareBy({ it.metricName }, { it.eventClassName })).forEach {
+        if (dump.unarySignificance[it] != 0.0) {
+            val mVal = "%.4f".format(dump.unarySignificance[it])
+            println("S_${it.metricName}(${it.eventClassName}) = $mVal")
+        }
+    }
+    dump.binarySignificance.keys.sortedWith(compareBy({ it.metricName }, { it.eventClass1Name }, { it.eventClass2Name })).forEach {
+        if (dump.binarySignificance[it] != 0.0) {
+            val mVal = "%.4f".format(dump.binarySignificance[it])
+            println("S_${it.metricName}(${it.eventClass1Name}, ${it.eventClass2Name}) = $mVal")
+        }
+    }
+    dump.binaryCorrelation.keys.sortedWith(compareBy({ it.metricName }, { it.eventClass1Name }, { it.eventClass2Name })).forEach {
+        if (dump.binaryCorrelation[it] != 0.0) {
+            val mVal = "%.4f".format(dump.binaryCorrelation[it])
+            println("C_${it.metricName}(${it.eventClass1Name}, ${it.eventClass2Name}) = $mVal")
+        }
+    }
+    println()
 }
