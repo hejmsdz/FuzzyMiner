3f8213c069243d50ccd0d9d99f0e8e1d5cf90a29
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-07-25 17:24:02 +0200

    :alembic: compare aggregated metrics

--- src/com/mrozwadowski/fuzzyminer/experiments/OnlineWindowComparison.kt
+++ src/com/mrozwadowski/fuzzyminer/experiments/OnlineWindowComparison.kt
@@ -3,18 +3,19 @@ package com.mrozwadowski.fuzzyminer.experiments
 import com.mrozwadowski.fuzzyminer.input.getLogReader
 import com.mrozwadowski.fuzzyminer.mining.FuzzyMiner
 import com.mrozwadowski.fuzzyminer.mining.metrics.MetricsStore
-import com.mrozwadowski.fuzzyminer.mining.metrics.minimalMetrics
+import com.mrozwadowski.fuzzyminer.mining.metrics.defaultMetrics
 import com.mrozwadowski.fuzzyminer.mining.online.OnlineFuzzyMiner
 import com.mrozwadowski.fuzzyminer.output.Dot
 import org.deckfour.xes.classification.XEventNameClassifier
 import java.io.File
+import kotlin.math.absoluteValue
 
 fun main() {
     val log = getLogReader(File("sampleData/journal_review.xes")).readLog()
     val window = SlidingWindow(log, windowSize = 20, stride = 5)
 
     val classifier = XEventNameClassifier()
-    val metrics = minimalMetrics()
+    val metrics = defaultMetrics()
     val onlineMiner = OnlineFuzzyMiner(classifier, metrics)
 
     onlineMiner.learn(window.initial())
@@ -25,10 +26,35 @@ fun main() {
     println(Dot(onlineMiner.graph))
 
     val lastFragment = window.fragment(window.steps().last)
-    val offlineMetrics = minimalMetrics()
+    val offlineMetrics = defaultMetrics()
     val offlineMiner = FuzzyMiner(lastFragment, classifier, offlineMetrics)
     val offlineGraph = offlineMiner.mine()
     println(Dot(offlineGraph))
+
+    compareMetrics(metrics, offlineMetrics)
+}
+
+fun compareMetrics(metrics1: MetricsStore, metrics2: MetricsStore) {
+    metrics1.aggregateUnarySignificance.forEach { (key, value) ->
+        val offlineValue = metrics2.aggregateUnarySignificance[key] ?: 0.0
+        if ((offlineValue - value).absoluteValue > 0.0001) {
+            println("US($key):\n    online[$value], offline[$offlineValue]")
+        }
+    }
+
+    metrics1.aggregateBinarySignificance.forEach { (key, value) ->
+        val offlineValue = metrics2.aggregateBinarySignificance[key] ?: 0.0
+        if ((offlineValue - value).absoluteValue > 0.0001) {
+            println("BS($key):\n    online[$value], offline[$offlineValue]")
+        }
+    }
+
+    metrics1.aggregateBinaryCorrelation.forEach { (key, value) ->
+        val offlineValue = metrics2.aggregateBinaryCorrelation[key] ?: 0.0
+        if ((offlineValue - value).absoluteValue > 0.0001) {
+            println("BC($key):\n    online[$value], offline[$offlineValue]")
+        }
+    }
 }
 
 fun printMetrics(metrics: MetricsStore) {
