a65076b9688277dbed53d69518d747c948a7a60f
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-07-26 21:14:38 +0200

    :alembic: compare graphs

--- src/com/mrozwadowski/fuzzyminer/experiments/OnlineWindowComparison.kt
+++ src/com/mrozwadowski/fuzzyminer/experiments/OnlineWindowComparison.kt
@@ -6,7 +6,6 @@ import com.mrozwadowski.fuzzyminer.mining.FuzzyMiner
 import com.mrozwadowski.fuzzyminer.mining.metrics.MetricsStore
 import com.mrozwadowski.fuzzyminer.mining.metrics.defaultMetrics
 import com.mrozwadowski.fuzzyminer.mining.online.OnlineFuzzyMiner
-import com.mrozwadowski.fuzzyminer.output.Dot
 import org.deckfour.xes.classification.XEventNameClassifier
 import java.io.File
 import kotlin.math.absoluteValue
@@ -16,24 +15,24 @@ fun main() {
     val window = SlidingWindow(log, windowSize = 20, stride = 5)
 
     val classifier = XEventNameClassifier()
-    val metrics = metricsFactory()
-    val onlineMiner = OnlineFuzzyMiner(classifier, metrics)
+    val onlineMetrics = metricsFactory()
+    val onlineMiner = OnlineFuzzyMiner(classifier, onlineMetrics)
 
     onlineMiner.learn(window.initial())
     window.steps().forEach { step ->
         onlineMiner.learn(window.incoming(step))
         onlineMiner.unlearn(window.outgoing(step))
+        val onlineGraph = onlineMiner.graph
+
+        val fragment = window.fragment(step)
+        val offlineMetrics = metricsFactory()
+        val offlineMiner = FuzzyMiner(fragment, classifier, offlineMetrics)
+        val offlineGraph = offlineMiner.mine()
+
+        println("=== STEP $step ===")
+        compareGraphs(onlineGraph, offlineGraph, verbose = true)
+        println()
     }
-    println(Dot(onlineMiner.graph))
-
-    val lastFragment = window.fragment(window.steps().last)
-    val offlineMetrics = metricsFactory()
-    val offlineMiner = FuzzyMiner(lastFragment, classifier, offlineMetrics)
-    val offlineGraph = offlineMiner.mine()
-    println(Dot(offlineGraph))
-
-    compareMetrics(metrics, offlineMetrics)
-    compareGraphs(onlineMiner.graph, offlineGraph)
 }
 
 fun metricsFactory() = defaultMetrics()
@@ -61,7 +60,9 @@ fun compareMetrics(metrics1: MetricsStore, metrics2: MetricsStore) {
     }
 }
 
-fun compareGraphs(onlineGraph: Graph, offlineGraph: Graph) {
+fun compareGraphs(onlineGraph: Graph, offlineGraph: Graph, verbose: Boolean = false) {
+    var difference = 0.0
+
     val nodes1 = onlineGraph.nodes.map { it.toString() to it }.toMap()
     val nodes2 = offlineGraph.nodes.map { it.toString() to it }.toMap()
     val edges1 = onlineGraph.allEdgeObjects().map { (it.source.toString() to it.target.toString()) to it }.toMap()
@@ -74,15 +75,18 @@ fun compareGraphs(onlineGraph: Graph, offlineGraph: Graph) {
         val node1 = nodes1[nodeName]
         val node2 = nodes2[nodeName]
         if (node1 == null) {
-            println("$nodeName missing in online graph")
+            difference += node2?.significance ?: 0.0
+            if (verbose) println("$nodeName missing in online graph")
             return@forEach
         }
         if (node2 == null) {
-            println("$nodeName excessive in online graph")
+            difference += node1.significance
+            if (verbose) println("$nodeName excessive in online graph")
             return@forEach
         }
         if ((node1.significance - node2.significance).absoluteValue > 0.0001) {
-            println("$nodeName has significance ${node1.significance} in online graph and ${node2.significance} in offline graph")
+            difference += (node1.significance - node2.significance)
+            if (verbose) println("$nodeName has significance ${node1.significance} in online graph and ${node2.significance} in offline graph")
         }
     }
 
@@ -90,19 +94,29 @@ fun compareGraphs(onlineGraph: Graph, offlineGraph: Graph) {
         val edge1 = edges1[edgeName]
         val edge2 = edges2[edgeName]
         if (edge1 == null) {
-            println("$edgeName [sig = ${edge2?.significance}, cor = ${edge2?.correlation}] missing in online graph")
+            difference += (edge2?.significance ?: 0.0) + (edge2?.correlation ?: 0.0)
+            if (verbose) println("$edgeName [sig = ${edge2?.significance}, cor = ${edge2?.correlation}] missing in online graph")
             return@forEach
         }
         if (edge2 == null) {
-            println("$edgeName [sig = ${edge1.significance}, cor = ${edge1.correlation}] excessive in online graph")
+            difference += edge1.significance + edge1.correlation
+            if (verbose) println("$edgeName [sig = ${edge1.significance}, cor = ${edge1.correlation}] excessive in online graph")
             return@forEach
         }
         if ((edge1.significance - edge2.significance).absoluteValue > 0.0001) {
-            println("$edgeName has significance ${edge1.significance} in online graph and ${edge2.significance} in offline graph")
+            difference += (edge1.significance - edge2.significance)
+            if (verbose) println("$edgeName has significance ${edge1.significance} in online graph and ${edge2.significance} in offline graph")
         }
         if ((edge1.correlation - edge2.correlation).absoluteValue > 0.0001) {
-            println("$edgeName has correlation ${edge1.correlation} in online graph and ${edge2.correlation} in offline graph")
+            difference += (edge1.correlation - edge2.correlation)
+            if (verbose) println("$edgeName has correlation ${edge1.correlation} in online graph and ${edge2.correlation} in offline graph")
         }
+    }
+
+    if (difference > 0.0) {
+        println("Total difference: $difference")
+    } else {
+        println("Perfect match!")
     }
 }
 
