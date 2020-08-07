68424aa0d1897aa5413a762413f4621709fd4ce5
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-08-07 19:21:28 +0200

    :bug: fix numerical stability issues

---
+++ src/com/mrozwadowski/fuzzyminer/utils/FloatUtils.kt
@@ -0,0 +1,11 @@
+package com.mrozwadowski.fuzzyminer.utils
+
+import kotlin.math.absoluteValue
+
+const val EPSILON = 0.00001
+
+fun significantlyGreater(a: Double, b: Double): Boolean = a - b > EPSILON
+
+fun significantlyLess(a: Double, b: Double): Boolean = b - a > EPSILON
+
+fun significantlyEqual(a: Double, b: Double): Boolean = (a - b).absoluteValue < EPSILON
