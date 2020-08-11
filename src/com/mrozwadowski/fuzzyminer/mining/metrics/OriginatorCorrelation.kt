213e36412aa002ce386047972678c590aeb55e19
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-08-11 19:18:54 +0200

    :bug: add null safety to originator correlation

--- src/com/mrozwadowski/fuzzyminer/mining/metrics/OriginatorCorrelation.kt
+++ src/com/mrozwadowski/fuzzyminer/mining/metrics/OriginatorCorrelation.kt
@@ -5,9 +5,12 @@ import org.deckfour.xes.model.XEvent
 
 class OriginatorCorrelation: LogBasedBinaryMetric(normalize = true) {
     override fun evaluate(previousEvent: XEvent, event: XEvent): Double {
+        val resource1 = previousEvent.attributes["org:resource"] ?: return 0.0
+        val resource2 = event.attributes["org:resource"] ?: return 0.0
+
         return levenshteinRatio(
-            (previousEvent.attributes["org:resource"] as XAttributeLiteral).value,
-            (event.attributes["org:resource"] as XAttributeLiteral).value
+            (resource1 as XAttributeLiteral).value,
+            (resource2 as XAttributeLiteral).value
         )
     }
 }
