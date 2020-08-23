3921a3b1665f10c5a7b0724eb3afc0ff01538968
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-08-23 20:17:32 +0200

    :bug: flip edge filter normalization scope

--- src/com/mrozwadowski/fuzzyminer/mining/simplification/EdgeFilter.kt
+++ src/com/mrozwadowski/fuzzyminer/mining/simplification/EdgeFilter.kt
@@ -8,7 +8,7 @@ import com.mrozwadowski.fuzzyminer.utils.significantlyLess
 class EdgeFilter(private val graph: Graph) {
     fun apply(utilityRatio: Double, cutoff: Double): Graph {
         val edgesToRemove = graph.nodes
-            .map { graph.edgesFrom(it) }
+            .map { graph.edgesTo(it) }
             .filterNot { it.isEmpty() }
             .flatMap { edges ->
                 relativeUtilities(edges, utilityRatio)
@@ -21,17 +21,14 @@ class EdgeFilter(private val graph: Graph) {
     }
 
     fun relativeUtilities(edges: Collection<Edge>, utilityRatio: Double): Map<Edge, Double> {
-        val utilities = edges.associateWith {
-            utility(it.source, it.target, utilityRatio)
-        }
+        val utilities = edges.associateWith { utility(it, utilityRatio) }
         val min = utilities.values.min()!!
         val max = utilities.values.max()!!
         val range = max - min
         return utilities.mapValues { (it.value - min) / range }
     }
 
-    private fun utility(a: Node, b: Node, utilityRatio: Double): Double {
-        val edge = graph.edgeBetween(a, b) ?: return 0.0
+    private fun utility(edge: Edge, utilityRatio: Double): Double {
         return utilityRatio * edge.significance + (1 - utilityRatio) * edge.correlation
     }
 }
--- test/com/mrozwadowski/fuzzyminer/mining/simplification/EdgeFilterTest.kt
+++ test/com/mrozwadowski/fuzzyminer/mining/simplification/EdgeFilterTest.kt
@@ -3,6 +3,7 @@ package com.mrozwadowski.fuzzyminer.mining.simplification
 import com.mrozwadowski.fuzzyminer.data.graph.Edge
 import com.mrozwadowski.fuzzyminer.data.graph.Graph
 import com.mrozwadowski.fuzzyminer.data.graph.PrimitiveNode
+import com.mrozwadowski.fuzzyminer.output.Dot
 import org.deckfour.xes.classification.XEventClass
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -14,14 +15,14 @@ internal class EdgeFilterTest {
     private val r = PrimitiveNode(XEventClass("r", 4), 0.7)
     private val s = PrimitiveNode(XEventClass("s", 5), 0.5)
 
-    private val apEdge = Edge(a, p, 1.0, 1.0)
-    private val aqEdge = Edge(a, q, 0.9, 0.2)
-    private val arEdge = Edge(a, r, 0.7, 0.8)
-    private val asEdge = Edge(a, s, 0.5, 0.3)
+    private val paEdge = Edge(p, a, 1.0, 1.0)
+    private val qaEdge = Edge(q, a,0.9, 0.2)
+    private val raEdge = Edge(r, a, 0.7, 0.8)
+    private val saEdge = Edge(s, a, 0.5, 0.3)
 
     private val graph = Graph(
         listOf(a, p, q, r, s),
-        mapOf(a to listOf(apEdge, aqEdge, arEdge, asEdge))
+        mapOf(a to listOf(paEdge, qaEdge, raEdge, saEdge))
     )
 
     private val edgeFilter = EdgeFilter(graph)
@@ -30,19 +31,19 @@ internal class EdgeFilterTest {
 
     @Test
     fun relativeUtilities() {
-        val values = edgeFilter.relativeUtilities(graph.edgesFrom(a), utilityRatio)
-        assertEquals(1.000, values.getValue(apEdge), 0.001)
-        assertEquals(0.250, values.getValue(aqEdge), 0.001)
-        assertEquals(0.583, values.getValue(arEdge), 0.001)
-        assertEquals(0.000, values.getValue(asEdge), 0.001)
+        val values = edgeFilter.relativeUtilities(graph.edgesTo(a), utilityRatio)
+        assertEquals(1.000, values.getValue(paEdge), 0.001)
+        assertEquals(0.250, values.getValue(qaEdge), 0.001)
+        assertEquals(0.583, values.getValue(raEdge), 0.001)
+        assertEquals(0.000, values.getValue(saEdge), 0.001)
     }
 
     @Test
     fun apply() {
         val simplifiedGraph = edgeFilter.apply(utilityRatio, cutoff)
-        assertNotNull(simplifiedGraph.edgeBetween(a, p))
-        assertNull(simplifiedGraph.edgeBetween(a, q))
-        assertNotNull(simplifiedGraph.edgeBetween(a, r))
-        assertNull(simplifiedGraph.edgeBetween(a, s))
+        assertNotNull(simplifiedGraph.edgeBetween(p, a))
+        assertNull(simplifiedGraph.edgeBetween(q, a))
+        assertNotNull(simplifiedGraph.edgeBetween(r, a))
+        assertNull(simplifiedGraph.edgeBetween(s, a))
     }
 }
